8c0a7a21b1271f3449454b06ad67edfde8af225e
Himali saini <himalisaini1512@gmail.com>
2022-04-09 17:08:39 +0530

    Profiles & Orders

--- myapp/posts/models.py
+++ myapp/posts/models.py
@@ -11,7 +11,11 @@ CATEGORY_CHOICES = (
 
 # Create your models here.
 class Item(models.Model):
-    organization = models.OneToOneField(ProfileModel,on_delete=models.CASCADE,null=True)
+    posting_organization = models.OneToOneField(ProfileModel,on_delete=models.CASCADE,null=True,related_name = 'posting_organization')
+    ordering_organization = models.OneToOneField(ProfileModel,on_delete=models.CASCADE,null=True,related_name = 'ordering_organization')
+    isOrdered = models.BooleanField(default=False,null=True)
+    isQualityOK = models.BooleanField(default=False,null=True)
+    isQuantityOK = models.BooleanField(default=False,null=True)
     description = models.CharField(max_length=100)
     quantity = models.FloatField()
     category = models.CharField(choices=CATEGORY_CHOICES, default='VEG',null=True,max_length=100)
